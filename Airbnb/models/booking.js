736994f67b773b1378e879ac12c1153076b58771
Léonard <picot.leonard@gmail.com>
2017-10-12 19:18:56 +0200

    Ajout de la page de location

--- Airbnb/models/booking.js
+++ Airbnb/models/booking.js
@@ -1,2 +1,11 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+var db = new sqlite3.Database('./databases/airbnb.db');
+
+
+module.exports.getBookingListForId = function (id,userId, callback) {
+    db.all("SELECT CASE WHEN user_id!="+userId+" THEN 'Indisponnible' ELSE 'Reservé' END as title, dateStart as start, dateEnd as end, CASE WHEN user_id!="+userId+" THEN 'red' ELSE 'green' END as color FROM booking WHERE place_id = ?;", [id], callback);
+}
+
+module.exports.AddBookingInfo = (dateStart, dateEnd, place, userId, callback) => {
+    db.run("INSERT INTO booking (place_id,user_id,dateStart,dateEnd) VALUES (?,?,?,?);", [place.id, userId, dateStart, dateEnd], callback);
+}
--- Airbnb/routes/place.js
+++ Airbnb/routes/place.js
@@ -1,39 +1,57 @@
 var router = require('express').Router();
 var modelPlace = require('../models/place');
+var modelBooking = require('../models/booking');
 
 //Get place 
-router.get('/:id', function(req, res, next) {
-    
-    modelPlace.getPlace(req.params.id,(err,rows)=>{
+router.get('/:id', function (req, res, next) {
+
+    modelPlace.getPlace(req.params.id, (err, rows) => {
         place = rows[0]
-        if(typeof place === 'undefined') {
+        if (typeof place === 'undefined') {
             return res.redirect('/place/');
         }
-        res.render('place/get', { data: place});    
+        res.render('place/get', {data: place});
     });
 });
 
 //Get booking information
-router.get('/:id/book', function(req, res, next) {
-
-    place = modelPlace.getPlace(req.params.id);
-    if(typeof place === 'undefined') {
-        res.redirect('/place/');
-        res.finished = true;
-        res.end();
+router.get('/:id/book', function (req, res, next) {
+    modelPlace.getPlace(req.params.id, (err, rows) => {
+        var place = rows[0]
+        modelBooking.getBookingListForId(place.id,req.session.userid, (err, rows) => {
+            if(!rows) {
+                rows = [];
+            }
+            res.render('place/booking', {dataPlace: place, dataBooking: rows})
+        })
+    });
+});
+//POST booking information
+router.post('/:id/book', function (req, res, next) {
+    console.log(req.body);
+    if (!req.body.dateStart || !req.body.dateEnd) {
+        return res.status(500).end();
     }
-    res.end("WIP");
-    //res.render('place/get', { data: place});
+    modelPlace.getPlace(req.params.id, (err, rows) => {
+        var place = rows[0];
+        modelBooking.AddBookingInfo(req.body.dateStart, req.body.dateEnd, place, req.session.userid, (err, rows) => {
+            if(err) {
+                console.log(err);
+                return res.status(500).end();
+            }
+            return res.status(200).end();
+        })
+    });
 });
 
 // Get place list
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
     // fetching list
-    modelPlace.getList((err,rows) => {
-        res.render('place/list', { data: rows})
+    modelPlace.getList((err, rows) => {
+        res.render('place/list', {data: rows})
     });
-    
+
 });
 
 module.exports = router;
