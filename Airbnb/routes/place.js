9c25a18969bfd05336c045c1bf99600e2442f59b
Léonard <picot.leonard@gmail.com>
2017-10-12 15:25:55 +0200

    Mise en place de sqlite + utilisation pour les places

--- Airbnb/routes/place.js
+++ Airbnb/routes/place.js
@@ -3,12 +3,14 @@ var modelPlace = require('../models/place');
 
 //Get place 
 router.get('/:id', function(req, res, next) {
-
-    place = modelPlace.getPlace(req.params.id);
-    if(typeof place === 'undefined') {
-        res.redirect('/place/');
-    }
-    res.render('place/get', { data: place});
+    
+    modelPlace.getPlace(req.params.id,(err,rows)=>{
+        place = rows[0]
+        if(typeof place === 'undefined') {
+            res.redirect('/place/');
+        }
+        res.render('place/get', { data: place});    
+    });
 });
 
 //Get booking information
@@ -26,15 +28,10 @@ router.get('/:id/book', function(req, res, next) {
 router.get('/', function(req, res, next) {
 
     // fetching list
-    var result = modelPlace.getList();
-    console.log(result);
-    res.render('place/list', { data: result});
-});
-
-// set booking
-router.get('/booking/:id', function(req, res, next) {
-
-    res.render('register', { title: 'YnovBnb' });
+    modelPlace.getList((err,rows) => {
+        res.render('place/list', { data: rows})
+    });
+    
 });
 
 module.exports = router;
